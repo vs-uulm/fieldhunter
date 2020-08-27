eb144ef280f293ab351478d7709b2ca41387c811
Stephan Kleber <stephan.kleber@uni-ulm.de>
2020-08-27 15:07:08 +0200

    eval cleanup

--- eval-fh.sh
+++ eval-fh.sh
@@ -3,15 +3,10 @@
 #input=input/*-100.pcap
 #input=input/*-1000.pcap
 #input="input/*-100.pcap input/*-1000.pcap"
-#input="input/ntp_SMIA-20111010_deduped-1000.pcap input/smb_SMIA20111010-one_deduped-1000.pcap"
-#input=input/ntp_SMIA-20111010_deduped-1000.pcap
-#input=input/smb_maccdc2012_maxdiff-1000.pcap
 #input=input/maxdiff-filtered/*-1000.pcap
-input=input/maxdiff-fromOrig/*-1000.pcap
-#input="input/maxdiff-fromOrig/smb_SMIA20111010-one-rigid1_maxdiff-1000.pcap
-#  input/maxdiff-fromOrig/ntp_SMIA-20111010_maxdiff-1000.pcap"
-#  input=input/maxdiff-fromOrig/dhcp_SMIA2011101X-filtered_maxdiff-1000.pcap
-#input=input/maxdiff-fromOrig/ntp_SMIA-20111010_maxdiff-100.pcap
+#input=input/maxdiff-fromOrig/*-1000.pcap
+input=input/maxdiff-fromOrig/*-100.pcap
+
 
 #tftnext=$(expr 1 + $(ls -d reports/tft-* | sed "s/^.*tft-\([0-9]*\)-.*$/\1/" | sort | tail -1))
 #tftnpad=$(printf "%03d" ${tftnext})
@@ -19,7 +14,7 @@ input=input/maxdiff-fromOrig/*-1000.pcap
 #report=reports/tft-${tftnpad}-clustering-${currcomm}
 #mkdir ${report}
 
-for fn in ${input} ; do python src/fh.py ${fn} ; done
+for fn in ${input} ; do python src/fh.py -r ${fn} ; done
 
 #mv reports/*.csv ${report}/
 #mv reports/*.pdf ${report}/
